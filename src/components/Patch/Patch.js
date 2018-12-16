17bec9365b0961a3a99e3b651585354fe0df2268
Karthik Manickam <karthik.omanickam@gmail.com>
2018-12-16 10:56:34 -0800

    Added Patching page to router

---
+++ src/components/Patch/Patch.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Patch extends Component {
+  render() {
+    return (
+      <h1 className="resume">Resume Patching Interface</h1>
+    );
+  }
+}
+
+export default Patch;
