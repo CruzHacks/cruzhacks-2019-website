22a7efe4bacfdf4f14fb1a46ac3a1193d4409024
Karthik Manickam <karthik.omanickam@gmail.com>
2019-01-16 21:20:18 -0800

    Added social media to live app

---
+++ src/components/Live/Announcements/Announcements.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Announcements extends Component {
+  render() {
+    return (
+        
+    );
+  }
+}
+
+export default Announcements;
--- src/components/Live/Live.js
+++ src/components/Live/Live.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
+
 import logo from 'assets/logo.svg';
+import instagram from 'assets/social/instagram.svg';
+import facebook from 'assets/social/facebook.svg';
+import twitter from 'assets/social/twitter.svg';
+
+import Announcements from './Announcements';
 
 class Live extends Component {
   render() {
     return (
-      <h1>Live App Lives Here</h1>
+        <div className="live-hero">
+            <img class="live-hero__logo" src={logo} alt="logo"/>
+            <h1 class="live-hero__title">CruzHacks 2019</h1>
+            <div className="live-hero__social">
+                <a href="https://www.instagram.com/cruzhacks/" target="_blank"><img src={instagram} alt=""/></a>
+                <a href="https://facebook.com/CruzHacks/" target="_blank"><img src={facebook} alt=""/></a>
+                <a href="https://twitter.com/CruzHacks" target="_blank"><img src={twitter} alt=""/></a>
+            </div>
+            <Announcements />
+        </div>
     );
   }
 }
