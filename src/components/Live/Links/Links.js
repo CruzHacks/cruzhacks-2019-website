b631ed6bf7a15e9c820606cdd6c0c87648cb81ff
Karthik Manickam <karthik.omanickam@gmail.com>
2019-01-16 23:32:11 -0800

    gnight squad

--- src/components/Live/Links/Links.js
+++ src/components/Live/Links/Links.js
@@ -4,12 +4,12 @@ class Links extends Component {
   render() {
     return (
         <div className="links-container">
-          <a href="#" className="link__item">Slack</a>
-          <a href="#" className="link__item">DevPost</a>
-          <a href="#" className="link__item">Hack Pack</a>
-          <a href="#" className="link__item">Map</a>
-          <a href="#" className="link__item">MLH</a>
-          <a href="#" className="link__item">Crowd Forge</a>
+          <a href="#" target="_blank" className="link__item">Slack</a>
+          <a href="#" target="_blank" className="link__item">DevPost</a>
+          <a href="#" target="_blank" className="link__item">Hack Pack</a>
+          <a href="#" target="_blank" className="link__item">Map</a>
+          <a href="#" target="_blank" className="link__item">MLH</a>
+          <a href="#" target="_blank" className="link__item">Crowd Forge</a>
         </div>
     );
   }
