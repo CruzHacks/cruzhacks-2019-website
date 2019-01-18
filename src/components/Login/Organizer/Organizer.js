f5318b99aa038f9f35a4d05116aa9144459239b6
Karthik Manickam <karthik.omanickam@gmail.com>
2019-01-18 10:25:50 -0800

    Created Organizer form component template

---
+++ src/components/Login/Organizer/Organizer.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Organizer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+      password: ""
+    }
+  }
+
+  handleOnChange = (event) => {
+    let newState = {}
+    const name = event.target.name
+    newState[name] = event.target.value
+    this.setState(newState, function() {
+      console.log(newState)
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event)
+  }
+
+  render() {
+    return (
+      <div className="organizer-form">
+        <form className="form" onSubmit={this.handleSubmit}>
+            <div className="form__group">
+              <input className="form__group__input" id="title" name="title" type="text" onChange={this.handleOnChange} value={this.state.title} maxLength="20" required/>
+              <label className={this.state.title ? "form__group__label" : "inactive form__group__label"} htmlFor="title">Title</label>
+            </div>
+            <div className="form__group">
+              <input className="form__group__input" id="password" name="password" type="password" onChange={this.handleOnChange} value={this.state.password} maxLength="20" required/>
+              <label className={this.state.password ? "form__group__label" : "inactive form__group__label"} htmlFor="password">Password*</label>
+            </div>              
+            <input className="form__submit" type="submit" value="Login"/>
+          </form>
+      </div>
+    );
+  }
+}
+
+export default Organizer;
