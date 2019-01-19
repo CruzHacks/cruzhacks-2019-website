211d9609b0ad0c9a4059f53a8307de70a346e6ac
Karthik Manickam <karthik.omanickam@gmail.com>
2019-01-19 12:35:11 -0800

    added few announcements

--- src/components/Live/Announcements/Announcements.js
+++ src/components/Live/Announcements/Announcements.js
@@ -9,12 +9,11 @@ class Announcements extends Component {
           <div className="announcements">
             <span className="announcements__title">Announcements</span>
             <HorziontalScroll className="announcements-container">
+              {/* <Announcement title="Lunch is being served!" day="Sat" time="1:00pm" /> */}
+              <Announcement title="Intro to Amazon Web Services workshop will be at Workshop A at 10:00AM" day="Sat" time="10:00am" />
+              <Announcement title="There will be yoga going on at the Overflow room at 9am" day="Sat" time="8:42am" />
+              <Announcement title="Need help? Text a mentor! Mentor hotline now open! Text `(832) 2-MENTOR`" day="" time="" />
               <Announcement title="Overflow Room Is Open for Hackers!" day="" time="" />
-              <Announcement title="" day="" time="" />
-              <Announcement title="" day="" time="" />
-              <Announcement title="" day="" time="" />
-              <Announcement title="" day="" time="" />
-              <Announcement title="" day="" time="" />
             </HorziontalScroll>
           </div>
     );
