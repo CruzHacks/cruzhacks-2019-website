fd7a7607b00560be9bcb439f61545de81da63d45
greg-wolff <gregwwolff@gmail.com>
2019-02-25 12:01:33 -0800

    env var fixed

--- src/Layout.js
+++ src/Layout.js
@@ -59,7 +59,7 @@ class Layout extends Component {
               }/>
               <Route path="/live" exact component={Live}/>
               {console.log(process.env, process)}
-              {sponsorData.map(el => <Route path={`/${el.name}`} exact render={() => <PostLanding data={sponsorTemplate(el).filter(e => e)} sponsor/>}/> )}
+              {sponsorData.map(el => <Route path={`/${uuidv5(el.name, String(process.env.SECRET_TOKEN || ''))}/${el.name}`} exact render={() => <PostLanding data={sponsorTemplate(el).filter(e => e)} sponsor/>}/> )}
               <Route path="/" component={Landing}/>
             </Switch>
           </Router>
